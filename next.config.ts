af8d40576a1dfa5bfeecdf7ee2a517b74d8e7ef2
gaoying <gaoying@9fbank.cc>
2024-10-28 14:26:56 +0800

    fix: 优化体积

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,14 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  compress: true,
+  images: {
+    unoptimized: false,
+  },
+  compiler: {
+    removeConsole: process.env.NODE_ENV === "production",
+  },
+  output: 'standalone',
 };
 
 export default nextConfig;
